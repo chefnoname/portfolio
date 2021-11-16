169facc7c0d95e567a34459f4b68c9090b0f5e4a
abdiWali <73367271+chefnoname@users.noreply.github.com>
2021-11-16 17:37:17 +0000

    Update Contact.js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -20,74 +20,81 @@ const Contact = ({ theme }) => {
 
   const isTabletOrMobile = useMediaQuery({ query: '(max-width: 760px)' });
 
-
   return (
-    <div id="contact">
-      <div className="seeMore">
-        <Typography
-          variant="h4"
-          sx={{
-            textAlign: "center",
-            color: theme,
-            fontWeight: "bolder",
-            width: "400px",
-          }}
-          className="contactsMediaQueries"
-        >
-          Want to see more? Get in Touch
-        </Typography>
-        <div className="contactLinks">
-          <a
-            href="https://github.com/chefnoname"
-            target="_blank"
-            className="githubPageLink"
-          >
-            <BsGithub />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/abdiwali-jama-71524a163/"
-            target="_blank"
-            className="linkedInPageLink"
-          >
-            <BsLinkedin />
-          </a>
-          <a href="mailto:abdijama21@gmail.com" className="emailLink">
-            <SiGmail />
-          </a>
-        </div>
-      </div>
-      <ScrollContainer>
-        <ScrollPage page={isTabletOrMobile ? 10 : 8}>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              height: "400px",
-            }}
-          >
-            <span style={{ fontSize: "40px" }}>
-              <br />
-
-              <Animator animation={MoveOut(600, 500)}>
-                <ContactCard img={jsCertificate} />
-              </Animator>
-              <Animator animation={MoveOut(500, 500)}>
-                <ContactCard img={codeQuestion1} />
-              </Animator>
-              <Animator animation={MoveOut(-350, -100)}>
-                <ContactCard img={firebase} />
-              </Animator>
-              <Animator animation={MoveOut(-350, -100)}>
-                <ContactCard img={jsCertificate} />
-              </Animator>
-              <br />
-            </span>
-          </div>
-        </ScrollPage>
-      </ScrollContainer>
+    <div>
+      <h1>HEY</h1>
     </div>
-  );
+  )
+
 };
 
 export default Contact;
+
+
+  // return (
+  //   <div id="contact">
+  //     <div className="seeMore">
+  //       <Typography
+  //         variant="h4"
+  //         sx={{
+  //           textAlign: "center",
+  //           color: theme,
+  //           fontWeight: "bolder",
+  //           width: "400px",
+  //         }}
+  //         className="contactsMediaQueries"
+  //       >
+  //         Want to see more? Get in Touch
+  //       </Typography>
+  //       <div className="contactLinks">
+  //         <a
+  //           href="https://github.com/chefnoname"
+  //           target="_blank"
+  //           className="githubPageLink"
+  //         >
+  //           <BsGithub />
+  //         </a>
+  //         <a
+  //           href="https://www.linkedin.com/in/abdiwali-jama-71524a163/"
+  //           target="_blank"
+  //           className="linkedInPageLink"
+  //         >
+  //           <BsLinkedin />
+  //         </a>
+  //         <a href="mailto:abdijama21@gmail.com" className="emailLink">
+  //           <SiGmail />
+  //         </a>
+  //       </div>
+  //     </div>
+  //     <ScrollContainer>
+  //       <ScrollPage page={isTabletOrMobile ? 10 : 8}>
+  //         <div
+  //           style={{
+  //             display: "flex",
+  //             justifyContent: "center",
+  //             alignItems: "center",
+  //             height: "400px",
+  //           }}
+  //         >
+  //           <span style={{ fontSize: "40px" }}>
+  //             <br />
+
+  //             <Animator animation={MoveOut(600, 500)}>
+  //               <ContactCard img={jsCertificate} />
+  //             </Animator>
+  //             <Animator animation={MoveOut(500, 500)}>
+  //               <ContactCard img={codeQuestion1} />
+  //             </Animator>
+  //             <Animator animation={MoveOut(-350, -100)}>
+  //               <ContactCard img={firebase} />
+  //             </Animator>
+  //             <Animator animation={MoveOut(-350, -100)}>
+  //               <ContactCard img={jsCertificate} />
+  //             </Animator>
+  //             <br />
+  //           </span>
+  //         </div>
+  //       </ScrollPage>
+  //     </ScrollContainer>
+  //   </div>
+  // );
